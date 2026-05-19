27fdcd08bc8a5e51bac4c23158ce859b999aadc5
wyvern623 <mucunhi3@gmail.com>
2026-05-19 21:21:05 +0900

    test: NN重みコピーとcloneの条件を追加

--- evolution/test_evolution.py
+++ evolution/test_evolution.py
@@ -30,6 +30,28 @@ def test_neural_net_rejects_wrong_input_shape() -> None:
         raise AssertionError("NeuralNet.forward should reject wrong input shape")
 
 
+def test_neural_net_set_weights_matches_forward_result() -> None:
+    net = NeuralNet()
+    copied_net = NeuralNet()
+    copied_net.set_weights(net.get_weights())
+
+    input_vec = np.linspace(0.0, 1.0, DEFAULT_INPUT_SIZE)
+    assert np.allclose(copied_net.forward(input_vec), net.forward(input_vec))
+
+
+def test_neural_net_clone_keeps_independent_weights() -> None:
+    net = NeuralNet()
+    cloned_net = net.clone()
+    cloned_weights = cloned_net.get_weights()
+
+    changed_weights = net.get_weights()
+    changed_weights[0][0, 0] += 1.0
+    net.set_weights(changed_weights)
+
+    assert not np.array_equal(cloned_net.w1, net.w1)
+    assert np.array_equal(cloned_net.w1, cloned_weights[0])
+
+
 def test_evolution_manager_uses_default_neural_net_shape() -> None:
     manager = EvolutionManager(population_size=2)
     assert manager.population[0].input_size == DEFAULT_INPUT_SIZE
