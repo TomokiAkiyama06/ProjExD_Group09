0ae00fcec9bbaafd399dfdf162bf1f15a513f54a
riku <c0a252622d@edu.teu.ac.jp>
2026-05-28 03:00:29 +0900

    Delay pygame import in font loader

--- core/fonts.py
+++ core/fonts.py
@@ -6,23 +6,16 @@ import os
 from functools import cache
 from pathlib import Path
 
-import pygame as pg
-
-# GitHub CI環境（画面なし）での強制終了(exit code 139)を防ぐためのダミードライバ設定
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 os.environ["SDL_AUDIODRIVER"] = "dummy"
-
-# フォントモジュールが未初期化の場合の安全装置
-if not pg.font.get_init():
-    pg.font.init()
 
 FONT_DIR = Path(__file__).parent.parent / "assets" / "font"
 FONT_DEFAULT = FONT_DIR / "NotoSansJP-Regular.ttf"
 
 
 @cache
-def get_font(size: int, path: Path = FONT_DEFAULT) -> pg.font.Font:
-    """指定サイズのフォントを返す（同サイズはキャッシュ）。"""
+def get_font(size: int, path: Path = FONT_DEFAULT):
+    import pygame as pg
 
     try:
         if not pg.font.get_init():
